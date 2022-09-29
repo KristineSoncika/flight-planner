02eada87a13f149bbffd2425982091e5b154746d
kristine <57680335+kristtiine@users.noreply.github.com>
2022-09-29 12:05:44 +0300

    fix adminapi validator

--- Validations/AdminApiValidator.cs
+++ Validations/AdminApiValidator.cs
@@ -8,42 +8,20 @@ public static class AdminApiValidator
 {
     public static bool HasInvalidValues(Flight flight)
     {
-        foreach(var flightProperty in flight.GetType().GetProperties())
-        {
-            if(flightProperty.PropertyType == typeof(string))
-            {
-                var value = (string)flightProperty.GetValue(flight);
-                if(string.IsNullOrWhiteSpace(value))
-                {
-                    return true;
-                }
-            }
-            
-            var from = flight.From;
-            var to = flight.To;
-            
-            if(flightProperty.PropertyType == typeof(Airport))
-            {
-                if (from.GetType().GetProperties().Select(fromProperty => (string)fromProperty.GetValue(from)).Any(string.IsNullOrWhiteSpace))
-                {
-                    return true;
-                }
-
-                if (to.GetType().GetProperties().Select(toProperty => (string)toProperty.GetValue(to)).Any(string.IsNullOrWhiteSpace))
-                {
-                    return true;
-                }
-            }
-        }
-        
-        return false;
+      return string.IsNullOrWhiteSpace(flight.From.Country) || 
+             string.IsNullOrWhiteSpace(flight.From.City) || 
+             string.IsNullOrWhiteSpace(flight.From.AirportCode) || 
+             string.IsNullOrWhiteSpace(flight.To.Country) || 
+             string.IsNullOrWhiteSpace(flight.To.City) || 
+             string.IsNullOrWhiteSpace(flight.To.AirportCode) || 
+             string.IsNullOrWhiteSpace(flight.Carrier) || 
+             string.IsNullOrWhiteSpace(flight.DepartureTime) || 
+             string.IsNullOrWhiteSpace(flight.ArrivalTime);
     }
 
     public static bool IsSameAirport(Flight flight)
     {
-        return flight.From.City.ToUpper().Trim() == flight.To.City.ToUpper().Trim() &&
-               flight.From.Country.ToUpper().Trim() == flight.To.Country.ToUpper().Trim() &&
-               flight.From.AirportCode.ToUpper().Trim() == flight.To.AirportCode.ToUpper().Trim();
+        return flight.From.AirportCode.ToUpper().Trim() == flight.To.AirportCode.ToUpper().Trim();
     }
 
     public static bool IsWrongDate(Flight flight)
